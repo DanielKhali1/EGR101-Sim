9a2e4b969ac62e46f1de817ffd814e7be8967912
crumpl07 <crumpst3r2000@gmail.com>
2021-10-03 19:45:28 -0400

    Switching from GUI library to UI

--- Unity_Project/Assets/Scripts/GUITest.cs
+++ Unity_Project/Assets/Scripts/GUITest.cs
@@ -12,7 +12,7 @@ public class GUITest : MonoBehaviour
     {
      
         //Make a background box
-        GUI.Box(new Rect(10, 10, 800, 600), "GUI Test");
+        GUI.Box(new Rect(10, 10, 1000, 600), "GUI Test");
 
         //Ultrasonic Sensor selection menu box
         GUI.Box(new Rect(20, 30, 200, 300), "Ultrasonic Sensor");
