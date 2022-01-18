d73fdd26a550735846c7484f3a179e436ec69b04
Danny <38340314+DanielKhali1@users.noreply.github.com>
2022-01-18 16:04:51 -0500

    I hate myself

--- Source/EGR101Beans/src/com/egr101sim/core/SimulationManager.java
+++ Source/EGR101Beans/src/com/egr101sim/core/SimulationManager.java
@@ -24,6 +24,7 @@ public class SimulationManager {
 	
 	
 	public SimulationManager() {
+		
 	}
 	
 	public void setup() {
--- Source/EGR101Beans/src/com/egr101sim/start/Main.java
+++ Source/EGR101Beans/src/com/egr101sim/start/Main.java
@@ -2,17 +2,26 @@ package com.egr101sim.start;
 
 import com.egr101sim.ui.MainUI;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+// TODO: this doesn't work fix this later
+
 public class Main {
 	
 	public static void main(String[] args) {
-			Runnable runnable = () -> { 
-				try {
-					new MainUI().start(new javafx.stage.Stage());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			};
-			Thread t = new Thread(runnable);
-			t.start();
+
+
+		new Thread(new Runnable() {
+		    @Override public void run() {
+		        Platform.runLater(new Runnable() {
+		            @Override public void run() {
+						try {
+							new MainUI().start(new javafx.stage.Stage());
+						} catch(Exception e) {
+							System.out.println("uh oh UWU u made an oopsie");
+						}
+		        }});
+		    }
+		    }).start();
 	}
 }
