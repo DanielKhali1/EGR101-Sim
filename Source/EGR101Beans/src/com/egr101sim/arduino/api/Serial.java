e750308c268df6c464c485b3e936ca74ced6471b
keelymashburn <39414616+keelymashburn@users.noreply.github.com>
2022-03-05 10:21:14 -0500

    updated serial class

--- Source/EGR101Beans/src/com/egr101sim/arduino/api/Serial.java
+++ Source/EGR101Beans/src/com/egr101sim/arduino/api/Serial.java
@@ -1,5 +1,16 @@
 package com.egr101sim.arduino.api;
 
-public class Serial {
-
+public class Serial 
+{
+	public static void println(String words)
+	{
+		
+	}
+	
+	public static void print(String words)
+	{
+		
+	}
+	
+	static String buffer = "";
 }
