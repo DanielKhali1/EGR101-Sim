908c86a8a5cf6e01c5c0ac1416b5decb8bfec0f9
keelymashburn <39414616+keelymashburn@users.noreply.github.com>
2022-04-17 13:52:26 -0400

    updated menu items

--- Source/EGR101Beans/src/com/egr101sim/ui/MainUI.java
+++ Source/EGR101Beans/src/com/egr101sim/ui/MainUI.java
@@ -32,6 +32,7 @@ import com.egr101sim.core.ApplicationManager;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -301,12 +302,8 @@ public class MainUI extends Application {
 		serialimage.setFitHeight(30);
 		serialimage.setFitWidth(35);
 
-		Button wiring = new Button("Wiring Interface");
-		wiring.relocate(840, 35);
-		wiring.setPrefSize(110, 30);
-
 		Button botCustomization = new Button("Bot Customization");
-		botCustomization.relocate(715, 35);
+		botCustomization.relocate(830, 35);
 		botCustomization.setPrefSize(120, 30);
 		
 		// new menu bar 
@@ -317,20 +314,21 @@ public class MainUI extends Application {
 		filebar.getItems().addAll(newf, open, save);
 		
 		Menu edit = new Menu("Edit");
-		MenuItem item2 = new MenuItem("");
-		edit.getItems().addAll(item2);
+		MenuItem idk = new MenuItem("Coming Soon");
+		edit.getItems().addAll(idk);
 		
 		Menu sketch = new Menu("Sketch");
-		MenuItem item3 = new MenuItem("");
-		sketch.getItems().addAll(item3);
+		MenuItem verify = new MenuItem("Verify/Compile");
+		MenuItem upload = new MenuItem("Upload");
+		sketch.getItems().addAll(verify, upload);
 		
 		Menu tools = new Menu("Tools");
-		MenuItem item4 = new MenuItem("");
-		tools.getItems().addAll(item4);
+		MenuItem monitor = new MenuItem("Serial Monitor");
+		tools.getItems().addAll(monitor);
 		
 		Menu help = new Menu("Help");
-		MenuItem item5 = new MenuItem("");
-		help.getItems().addAll(item5);
+		MenuItem helpitem = new MenuItem("Help");
+		help.getItems().addAll(helpitem);
 		
 		MenuBar menuBar = new MenuBar();
 		menuBar.setTranslateX(0);
@@ -339,23 +337,11 @@ public class MainUI extends Application {
 		menuBar.getMenus().addAll(filebar, edit, sketch, tools, help);
 
 		pane.getChildren().addAll(rectanglefix, rectangle4, rectangle, rectangle3, rectangle6, codeArea, rectangle2, t, rectangle5,
-				wiring, botCustomization, scrollPane, console, menuBar);
+			 botCustomization, scrollPane, console, menuBar);
 
 		pane.getChildren().addAll(runimage, buildimage, newimage, saveimage, openimage, serialimage);
 
 		scene = new Scene(pane, 1000, 760);
-
-		wiring.setOnAction(e -> {
-			try {
-				Runtime runTime = Runtime.getRuntime();
-				String executablePath = "..\\..\\Executables\\wiring\\Unity_Project.exe";
-				Process process = runTime.exec(executablePath);
-				// new WiringGUI(manager).start(new Stage());
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		});
 
 		botCustomization.setOnAction(e -> {
 			try {
@@ -367,6 +353,18 @@ public class MainUI extends Application {
 			}
 		});
 
+		verify.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	if (!manager.isSimRunning()) {
+					manager.updateBehavior(codeArea.getText(), console);
+					scrollPane.setVvalue(scrollPane.getVmax());
+				}
+				// console output
+				console.setText(console.getText() + "\n" + manager.stackPrint());
+				scrollPane.setVvalue(scrollPane.getVmax());
+		    }
+		});
+		
 		buildimage.setOnMouseClicked(e -> {
 			if (!manager.isSimRunning()) {
 				manager.updateBehavior(codeArea.getText(), console);
@@ -375,6 +373,43 @@ public class MainUI extends Application {
 			// console output
 			console.setText(console.getText() + "\n" + manager.stackPrint());
 			scrollPane.setVvalue(scrollPane.getVmax());
+		});
+		
+		upload.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	if (manager.simManager.getArduino().behavior.getFunction() != null) {
+					try {
+						Runtime runTime = Runtime.getRuntime();
+						String executablePath = "..\\..\\Executables\\simulation\\Unity_Project.exe";
+						process = runTime.exec(executablePath);
+
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+
+					thread = (new Thread(() -> {
+						manager.setSimRunning(true);
+						try {
+							manager.execute(process, console);
+							console.setText(console.getText() + "\n" + Serial.serialLog);
+							scrollPane.setVvalue(scrollPane.getVmax());
+						} catch (NullPointerException e3) {
+							process.destroy();
+							manager.setSimRunning(false);
+							manager.simManager.shutDown(console);
+							scrollPane.setVvalue(scrollPane.getVmax());
+						}
+
+						System.out.println("arduino execution thread over");
+					}));
+
+					thread.start();
+				} else {
+					console.setText("Verify before running!");
+					scrollPane.setVvalue(scrollPane.getVmax());
+				}
+		    }
+		    //manager.setSimRunning(false);
 		});
 
 		runimage.setOnMouseClicked(e -> {
@@ -417,6 +452,24 @@ public class MainUI extends Application {
 			});
 			manager.setSimRunning(false);
 		});
+		
+		save.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	try {
+					FileChooser fileChooser1 = new FileChooser();
+					fileChooser1.setInitialFileName(tempName);
+					File file = fileChooser1.showSaveDialog(primaryStage);
+					FileWriter fw = new FileWriter(file);
+					String code = codeArea.textProperty().getValue();
+					fw.write(code);
+					fw.close();
+
+				} catch (Exception e2) {
+					console.setText("ERROR: Unable to save file");
+					scrollPane.setVvalue(scrollPane.getVmax());
+				}
+		    }
+		});
 
 		saveimage.setOnMouseClicked(e -> {
 			try {
@@ -434,6 +487,31 @@ public class MainUI extends Application {
 			}
 		});
 
+		open.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	try {
+					FileChooser fileChooser2 = new FileChooser();
+					File Opened = fileChooser2.showOpenDialog(primaryStage);
+
+					BufferedReader in = new BufferedReader(new FileReader(Opened));
+					String str = "";
+					String code = "";
+
+					while ((str = in.readLine()) != null) {
+						code = code + str + "\n";
+					}
+
+					codeArea.clear();
+					codeArea.replaceText(0, 0, code);
+					codeArea.relocate(0, 100);
+					codeArea.setPrefSize(1000, 400);
+				} catch (Exception e2) {
+					console.setText("ERROR: Unable to open file");
+					scrollPane.setVvalue(scrollPane.getVmax());
+				}
+		    }
+		});
+		
 		openimage.setOnMouseClicked(e -> {
 			try {
 				FileChooser fileChooser2 = new FileChooser();
@@ -455,6 +533,34 @@ public class MainUI extends Application {
 				console.setText("ERROR: Unable to open file");
 				scrollPane.setVvalue(scrollPane.getVmax());
 			}
+		});
+		
+		newf.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	try {
+					Date date2 = new Date();
+					String tempName2 = "sketch_" + date2.getTime() + ".ino";
+
+					codeArea.clear();
+					codeArea.replaceText(0, 0, startingcode);
+
+					Text t2 = new Text();
+					t.setX(40);
+					t.setY(90);
+					t.setText(tempName2);
+					t.setFill(textGreen);
+
+					pane.getChildren().addAll(rectanglefix, t2);
+					Stage stage2 = new Stage();
+					stage2.setScene(scene);
+					stage2.setTitle("EGR101 Simulation Software");
+					stage2.show();
+				} catch (Exception e2) {
+					console.setText("ERROR: Unable to create new file");
+					scrollPane.setVvalue(scrollPane.getVmax());
+					e2.printStackTrace();
+				}
+		    }
 		});
 
 		newimage.setOnMouseClicked(e -> {
@@ -481,6 +587,56 @@ public class MainUI extends Application {
 				scrollPane.setVvalue(scrollPane.getVmax());
 				e2.printStackTrace();
 			}
+		});
+		
+		monitor.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	try {
+					System.out.println("serial clicked");
+					
+					Text serial = new Text();
+					serial.setX(10);
+					serial.setY(20);
+					serial.setText("Serial Monitor \n\n");
+					serial.setFill(Color.BLACK);
+					
+					Rectangle rectangle0 = new Rectangle();
+					rectangle0.setFill(Color.WHITE);
+					rectangle0.setX(0);
+					rectangle0.setY(0);
+					rectangle0.setWidth(300);
+					rectangle0.setHeight(600);
+					
+					ScrollPane scrollPane2 = new ScrollPane();
+			        scrollPane2.setStyle("-fx-background: #ffffff;");
+			        scrollPane2.setFitToWidth(true);
+			        scrollPane2.setPrefWidth(300);
+			        scrollPane2.setPrefHeight(600);
+			        scrollPane2.setTranslateX(0);
+			        scrollPane2.setTranslateY(0);
+			        scrollPane2.setContent(serial);
+			        serial.wrappingWidthProperty().bind(rectangle0.widthProperty());
+			        scrollPane2.setVvalue(scrollPane2.getVmax());
+			        
+			        final Popup popupwindow = new Popup();
+					popupwindow.setX(1000);
+			        popupwindow.setY(165);
+			        popupwindow.setWidth(300);
+			        popupwindow.setHeight(600);
+			        popupwindow.getContent().addAll(rectangle0, scrollPane2, serial);
+			        
+					popupwindow.show(primaryStage);
+					
+					//find better way to update i guess 
+				    serial.setText(Serial.serialLog);
+				    scrollPane2.setVvalue(scrollPane2.getVmax());
+					
+				} catch (Exception e2) {
+					console.setText("ERROR: Unable to open Serial Monitor");
+					scrollPane.setVvalue(scrollPane.getVmax());
+					e2.printStackTrace();
+				}
+		    }
 		});
 		
 		serialimage.setOnMouseClicked(e -> {
